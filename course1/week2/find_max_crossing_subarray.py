de5dd931357ab2a2b9f7b1fc804a2bea50066b5c
sherrylearn <sherryqixuan@ou.edu>
2017-03-08 18:07:06 -0600

    maxsum

--- course1/week2/find_max_crossing_subarray.py
+++ course1/week2/find_max_crossing_subarray.py
@@ -14,3 +14,26 @@ def find_max_crossing_subarray(A,low,mid,high):
             right_sum = sum
             max_right = j
     return(max_left,max_right,left_sum,right_sum)
+#class maxsum:
+#    def __init__(self,first,last,pay):
+
+def find_maximum_subarray(alist,low,high):
+
+    if high==low:
+        return(low,high,alist[low])
+    else:
+        mid = (low+high)/2
+        (left_low,left_high,left_sum) = find_maximum_subarray(alist,low,mid)
+        (right_low, right_high, right_sum) = find_maximum_subarray(alist,mid,high)
+        (cross_low,cross_high,cross_sum) = find_max_crossing_subarray(alist,low,mid,high)
+    if left_sum>=right_sum and left_sum>=cross_sum:
+        return (left_low,left_high,left_sum)
+    if right_sum>=left_sum and right_sum>=cross_sum:
+        return (right_low,right_high,right_sum)
+    else:
+        return (cross_low,cross_high,cross_sum)
+
+
+
+
+
